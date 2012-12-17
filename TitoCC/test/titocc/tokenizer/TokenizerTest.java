ac7942cb11f41a293d95f6e6544dbda4b2d2f995
tta <fc6fa0e9@nomail.com>
2012-12-17 22:54:37 +0200

    Tokenizer tests.

--- TitoCC/test/titocc/tokenizer/TokenizerTest.java
+++ TitoCC/test/titocc/tokenizer/TokenizerTest.java
@@ -1,5 +1,8 @@
 package titocc.tokenizer;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -34,7 +37,55 @@ public class TokenizerTest
 	}
 
 	@Test
-	public void test()
+	public void returnsCorrectTokens() throws IOException, SyntaxException
 	{
+		Tokenizer t = new Tokenizer(new StringReader(
+				"if(x == 0)\n"
+				+ "\tx = 1;"));
+		List<Token> tokens = t.tokenize();
+
+		assertTrue("if", tokens.get(0) instanceof KeywordToken);
+		assertEquals("if", "if", tokens.get(0).toString());
+
+		assertTrue("(", tokens.get(1) instanceof PunctuatorToken);
+		assertEquals("(", "(", tokens.get(1).toString());
+
+		assertTrue("x", tokens.get(2) instanceof IdentifierToken);
+		assertEquals("x", "x", tokens.get(2).toString());
+
+		assertTrue("==", tokens.get(3) instanceof PunctuatorToken);
+		assertEquals("==", "==", tokens.get(3).toString());
+
+		assertTrue("0", tokens.get(4) instanceof IntegerLiteralToken);
+		assertEquals("0", "0", tokens.get(4).toString());
+
+		assertTrue(")", tokens.get(5) instanceof PunctuatorToken);
+		assertEquals(")", ")", tokens.get(5).toString());
+
+		assertTrue("x", tokens.get(6) instanceof IdentifierToken);
+		assertEquals("x", "x", tokens.get(6).toString());
+
+		assertTrue("=", tokens.get(7) instanceof PunctuatorToken);
+		assertEquals("=", "=", tokens.get(7).toString());
+
+		assertTrue("1", tokens.get(8) instanceof IntegerLiteralToken);
+		assertEquals("1", "1", tokens.get(8).toString());
+
+		assertTrue(";", tokens.get(9) instanceof PunctuatorToken);
+		assertEquals(";", ";", tokens.get(9).toString());
+
+		assertTrue("<eof>", tokens.get(10) instanceof EofToken);
+		assertEquals("<eof>", "", tokens.get(10).toString());
+	}
+
+	@Test
+	public void throwsWhenUnrecognizedTokens() throws IOException, SyntaxException
+	{
+		try {
+			Tokenizer t = new Tokenizer(new StringReader("x = 1; #"));
+			t.tokenize();
+			fail("SyntaxException not thrown.");
+		} catch (SyntaxException e) {
+		}
 	}
 }
