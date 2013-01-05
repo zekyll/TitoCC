1ec1fd3c65c98259c73d4fc8d8389f9ac419d54b
tta <fc6fa0e9@nomail.com>
2013-01-05 18:32:00 +0200

    Simplified InternalCompilerException class.

--- TitoCC/src/titocc/compiler/InternalCompilerException.java
+++ TitoCC/src/titocc/compiler/InternalCompilerException.java
@@ -1,25 +1,18 @@
 package titocc.compiler;
 
 /**
- * Exception class for internal compiler errors. Should hopefully never
- * happen and the purpose is just to help detect bugs.
+ * Exception class for internal compiler errors. Should hopefully never happen
+ * and the purpose is just to help detect bugs.
  */
 public class InternalCompilerException extends RuntimeException
 {
-	private String message;
-
 	/**
 	 * Constructor.
+	 *
 	 * @param message error message for the exception
 	 */
 	public InternalCompilerException(String message)
 	{
-		this.message = message;
-	}
-
-	@Override
-	public String getMessage()
-	{
-		return message;
+		super(message);
 	}
 }
