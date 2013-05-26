04a21fcf5e9dcbe622726eebaea8f087127b0eef
Timo <fc6fa0e9@nomail.com>
2013-05-26 07:33:29 +0300

    Complete list of C keywords.

--- TitoCC/src/titocc/tokenizer/KeywordToken.java
+++ TitoCC/src/titocc/tokenizer/KeywordToken.java
@@ -9,42 +9,49 @@ import java.util.Set;
  */
 public class KeywordToken extends WordToken
 {
+	/**
+	 * List of keywords defined in C99 standard ($6.4.1).
+	 */
 	private static final String[] keywords = {
-		// In use:
-		"while",
-		"if",
-		"else",
-		"return",
-		"int",
-		"void",
-		"for",
-		// Reserved:
-		"do",
-		"break",
-		"continue",
-		"goto",
-		"const",
-		"volatile",
-		"unsigned",
-		"signed",
-		"char",
-		"short",
-		"long",
-		"float",
-		"double",
-		"enum",
-		"struct",
-		"union",
-		"switch",
-		"case",
-		"default",
-		"static",
-		"extern",
 		"auto",
+		"break",
+		"case",
+		"char",
+		"const",
+		"continue",
+		"default",
+		"do",
+		"double",
+		"else",
+		"enum",
+		"extern",
+		"float",
+		"for",
+		"goto",
+		"if",
+		"inline",
+		"int",
+		"long",
 		"register",
+		"restrict",
+		"return",
+		"short",
+		"signed",
 		"sizeof",
-		"typedef"
+		"static",
+		"struct",
+		"switch",
+		"typedef",
+		"union",
+		"unsigned",
+		"void",
+		"volatile",
+		"while",
+		"_Bool",
+		"_Complex",
+		"_Imaginary"
 	};
+
 	private static Set<String> keywordSet = new HashSet<String>(Arrays.asList(keywords));
 
 	/**
@@ -53,7 +60,7 @@ public class KeywordToken extends WordToken
 	 * @param string keyword string
 	 * @param line line number where the token is located
 	 * @param column column number where the token is located
-	 */	
+	 */
 	public KeywordToken(String string, int line, int column)
 	{
 		super(string, line, column);
